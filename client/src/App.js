d14bad0446b10dbe9188ab8a77685163db4fe376
redtogreen <90275131+redtogreen@users.noreply.github.com>
2025-08-07 15:15:18 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+ import React, { useState, useEffect } from 'react';
 import Chat from './components/Chat';
 import Library from './components/Library';
 import Dashboard from './components/Dashboard';
@@ -28,4 +28,4 @@ function App() {
   );
 }
 
-export default App;
+export default App; 
